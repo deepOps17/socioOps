36a46a95bf69a37ccf95b6d68b70fba5e8523e32
deepOps17 <deogharia.rajdeep@outlook.com>
2022-10-17 15:57:03 +0530

    creating ci/cd pipeline

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -19,11 +19,11 @@ def test_unauthorized_user_get_one_post(client, test_posts):
     res = client.get(f"/posts/{test_posts[0].id}")
     assert res.status_code == 401
 
-# def test_get_one_post(authorized_client, test_posts):
-#     res = authorized_client.get(f"/posts/{test_posts[0].id}")
-#     # post = schemas.PostOut(**res.json())
-#     # assert post.Post.id == test_posts[0].id
-#     assert res.status_code == 200
+def test_get_one_post(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/{test_posts[0].id}")
+    post = schemas.PostOut(**res.json())
+    assert post.Post.id == test_posts[0].id
+    assert res.status_code == 200
 
 @pytest.mark.parametrize("title, content, published",[
     ("awesome title", "awesome content", True),
